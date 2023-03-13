e6585a6b6b71cf6c2274a382243a06ffce731c12
Alisa Chentsova <minchina.alice@gmail.com>
2023-03-13 23:11:05 +0400

    slide info upgrade

--- index.js
+++ index.js
@@ -102,14 +102,13 @@ const superArr = ['Бэтмен', 'Супермен', 'Железный чело
 //готовим объект
 const superDescription = {
     'Бэтмен': {
-        description: "о Бэтмене",
-
         superHeroePic: "pics/batman.jpeg!!!!!!!!!!!!!!!!!!!!!!",
         comicsUniverse: "DC Comics", 
         alterEgo: "Брюс Уэйн", 
         typeOfActivity: "борец с преступностью, филантроп, миллиардер", 
         friends: "Робин, Бэтгерл", 
-        superPowers: "интеллект, обширные познания, знания боевых искусств, ловкость"
+        superPowers: "интеллект, обширные познания, знания боевых искусств, ловкость",
+        moreDetailed: "по популярности человек-летучая мышь может сравниться только с Суперменом. Его образ кажется очень мистическим и мрачным: черный костюм с развевающимся плащом, устрашающий Бэтмобиль, штаб-квартира в сырой пещере. Его биография настолько же темна, как и образ. В детстве у него на глазах убили родителей, и юный Брюс поклялся всеми силами защищать родной Готэм от преступности.<br>Брюс знаменит, несмотря на пожилой возраст: первый выпуск с его участием вышел аж в 1939 году. С каждым годом популярность только крепнет. Большую роль в этом сыграли экранизации: «Темный рыцарь», сериал «Готэм» и несколько одноименных картин. Главная причина такой любви среди фанатов — мысль, что каждый может стать Бэтменом.<br>Если подумать, это один из немногих героев, кто добился своего положения за счет ума и упорства, а не за счет мутаций."
     },
     'Супермен': {
         description: "о Супермене",
@@ -117,7 +116,6 @@ const superDescription = {
     }
 };
 //console.log(superDescription['Бэтмен'].description);
-//console.log(superDescription['Супермен'].image);
 
 //задаем первый индекс для отсчета
 let index = -1;
@@ -131,19 +129,28 @@ slideNext();
 
 
 function showSlide() {
-    //получаем данные карточки героя
+        //получаем данные карточки героя
         //имя берется поочередности из массива с именами, заложенного в переменной 
         let heroeName = superArr[index];
-        //console.log(heroeName, index);
         // чтение полей/ключей из объекта(распарсеной строки json, заложенной в константе superHeroesBase)
         const universe = superDescription[heroeName].comicsUniverse;
         const alterEgo = superDescription[heroeName].alterEgo;
         const activity = superDescription[heroeName].typeOfActivity;
         const friends = superDescription[heroeName].friends;
         const powers = superDescription[heroeName].superPowers;
+        const description = superDescription[heroeName].moreDetailed;
         const image = superDescription[heroeName].superHeroePic;
     
-        const contentHtml = ``
+        const contentHtml = `<div class="slider__main-text">
+        <h2 class="slider__card-name">${heroeName}</h2>
+        <div class="details"><b>Вселенная:</b> ${universe}</div>
+        <div class="details"><b>Альтер эго:</b> ${alterEgo}</div>
+        <div class="details"><b>Род деятельности:</b> ${activity}</div>
+        <div class="details"><b>Друзья:</b> ${friends}</div>
+        <div class="details"><b>Суперсилы:</b> ${powers}</div>
+        <div class="details"><b>Подробнее:</b> ${description}</div>
+        </div>
+        <img src="${image}" class="slider__card-image" alt="Batman / DC comics" />`
 
         //куда мы будем вставлять данные карточки героя
         document.getElementById("super-slider-card").innerHTML = contentHtml;
@@ -160,7 +167,7 @@ function slideNext() {
         //когда текущий индекс (переменная index) доходит до конца массива, то текущий индекс (переменная index) становится нулевым (index = 0)
         if (index == superArr.length-1) {
             index = 0;
-            // в других случаях текущий индекс (переменная index) станет следующим слайдом (index+1)
+        // в других случаях текущий индекс (переменная index) станет следующим слайдом (index+1)
         } else (index = index+1);
         showSlide()
 }
@@ -170,7 +177,7 @@ function slidePrev() {
     //когда текущий индекс (переменная index) становится на исходную точку (index == 0), то текущий индекс (переменная index) перелистывается в обратную сторону/с первого слайда на последний (index = superArr.length-1)
     if (index == 0) {
     index = superArr.length-1
-        //в других случаях текущий индекс (переменная index) станет предыдущим слайдом (index-1)
+    //в других случаях текущий индекс (переменная index) станет предыдущим слайдом (index-1)
     } else (index = index-1);
     showSlide()
 }
